c9a3e1e9a325ac1cdeb3c0e0e7531d7b37ccb829
richzago97 <rich.czago@gmail.com>
2023-05-17 19:00:04 -0300

    feat: concluido a logica das função sumIfFirstIsBigger

---
+++ desafios/number/8kyu/sumIfFirstIsBigger.ts
@@ -0,0 +1,14 @@
+/*
+    Desenvolva uma função que some dois números passados por parâmetro. 
+    A soma só deve acontecer se o primeiro número passado por parâmetro for maior que o segundo.
+    Caso contrário você deverá retornar a seguinte frase: 
+    "O primeiro número não é maior que o segundo."
+*/
+const sumIfFirstIsBigger = (num1: number, num2: number): number | string => {
+
+    if(num1 > num2){
+        return num1 + num2
+    }else {
+        return "O primeiro número não é maior que o segundo!"
+    }
+}
