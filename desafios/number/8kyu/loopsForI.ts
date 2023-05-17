f7f1221147dcd787658ed79750b00efa09907ba0
richzago97 <rich.czago@gmail.com>
2023-05-17 18:59:39 -0300

    feat: concluido a logica das funções de loop for I

---
+++ desafios/number/8kyu/loopsForI.ts
@@ -0,0 +1,51 @@
+/*
+  Escreva uma função chamada renderUpToTwenty. Quando a função for chamada, os números de 0 a 20 
+  devem ser impressos no console.
+*/
+const renderUpToTwenty = () => {
+    for(let i = 0; i <= 20; i++){
+        console.log(i)
+    }
+}
+
+const renderUpToTwenty2 = () => {
+    let number = 0
+    while(number <= 20){
+        console.log(number++)
+    }
+}
+
+/*
+  Escreva uma função chamada tenInTenToAHundred(). Quando a função for chamada, os números de 
+  10 a 100, contando de 10 em 10, devem ser impressos no console.
+*/
+
+const tenInTenToAHundred = () => {
+    for(let i = 10; i <= 100; i += 10){
+        console.log(i)
+    }
+}
+
+/*
+  Escreva uma função chamada oddUpToTwenty(). Quando a função for chamada, apresente no console 
+  os valores entre 0 e 20 que forem ímpares.
+*/
+
+const oddUpToTwenty = (): void => {
+    for (let i = 1; i <= 20; i += 2) {
+      console.log(i);
+    }
+  };
+
+/*
+  Escreva uma função chamada evenUpToTwenty(). Quando a função for chamada, apresente no console 
+  os valores entre 0 e 20 que forem pares.
+*/
+
+const evenUpToTwenty = (): void => {
+    for (let i = 0; i <= 20; i += 2) {
+      console.log(i);
+    }
+  };
+
+  
