e9fcb8e8dfd0ae3bcc770206317f73ce6bd10eb9
richzago97 <rich.czago@gmail.com>
2023-05-17 19:03:51 -0300

    feat: concluido a logica das função startsWithLeter

---
+++ desafios/string/8kyu/startsWithLetter.ts
@@ -0,0 +1,13 @@
+/*
+    Desenvolva uma função com 2 parâmetros. O primeiro deve se chamar 
+    palavra, e o segundo deve se chamar letra. Se a palavra passada no 
+    primeiro parâmetro começar com a letra passada como segundo 
+    parâmetro, retorne true. Se a palavra passada no primeiro 
+    parâmetro não começar com a letra passada no segundo parâmetro, 
+    retorne false.
+*/
+const startsWithLetter  = (palavra: string, letra:string): boolean => {
+    return palavra[0] === letra[0]
+}
+console.log(startsWithLetter ("Richard", "Bicardo"));
+
