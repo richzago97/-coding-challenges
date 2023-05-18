108dccf7394b7638b1c69d6e6ab75038a1a73f27
richzago97 <rich.czago@gmail.com>
2023-05-18 10:58:51 -0300

    feat: adicionado export nas funções

--- desafios/number/8kyu/loopForII.ts
+++ desafios/number/8kyu/loopForII.ts
@@ -3,7 +3,7 @@
     existentes na faixa de valores de 15 a 200.
 */
 
-const squareIntegers = () => {
+export const squareIntegers = () => {
     for(let i = 15; i <= 200; i++){
         const square = i * i
         console.log(square)
@@ -14,7 +14,7 @@ const squareIntegers = () => {
     Escreva uma função que apresente a soma dos cem primeiros números naturais.
 */
 
-const sumFirstHundredNumbers  = () => {
+export const sumFirstHundredNumbers  = () => {
     let sum = 0;
     for(let i = 0; i <= 100; i++){
         sum += i
@@ -27,7 +27,7 @@ const sumFirstHundredNumbers  = () => {
     que são divisíveis por 4.
 */
 
-const numbersDivisibleByFour  = () => {
+export const numbersDivisibleByFour  = () => {
     for(let i = 0; i < 200; i++){
         if(i % 4 == 0){
             console.log(i)
@@ -39,7 +39,7 @@ const numbersDivisibleByFour  = () => {
     Escreva uma função que calcule o resultado da soma e da média aritmética dos valores 
     pares situados na faixa numérica de 50 até 70.
 */
-const calculateSumAndAverage = () => {
+export const calculateSumAndAverage = () => {
     let sum = 0
     let count = 0
     for(let i = 50; i <= 70; i++){
@@ -49,8 +49,6 @@ const calculateSumAndAverage = () => {
         }
     }
     const media_arit = sum / count 
-
-    console.log(`A soma é ${sum} e a média é ${media_arit}.`);
 }
 
 
