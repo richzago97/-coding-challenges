80671a065af357d8293e483f578ceba8e2e0bd13
richzago97 <rich.czago@gmail.com>
2023-05-18 10:59:51 -0300

    feat: adicionado export nas funções

--- desafios/string/7kyu/isPalindromo.ts
+++ desafios/string/7kyu/isPalindromo.ts
@@ -1,9 +1,6 @@
 // Crie uma função que verifique se a string recebida é um palindromo
-const isPalindromo = (word: string): Boolean => {
+export const isPalindromo = (word: string): Boolean => {
     return word === word.split("").reverse().join("");
 }
 
 
-console.log("A palavra é palindromo?")
-console.log(isPalindromo("Arara"));
-console.log("****************")
