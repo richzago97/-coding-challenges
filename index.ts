89cf623ca8bc24f27d7b1b89686351088a510a8a
richzago97 <rich.czago@gmail.com>
2023-05-18 11:01:13 -0300

    feat: criado o index da aplicação onde será chamado as funções

---
+++ index.ts
@@ -0,0 +1,17 @@
+//numbers
+import { fibonacciNumbers, fibonacci, sumGrains } from "./desafios/number/7kyu/loopForIII";
+import { checkHourAvailable } from "./desafios/number/8kyu/checkHourAvailable";
+import { isBigger, isBigger2 } from "./desafios/number/8kyu/isBigger";
+import { isPositiveOrNegative, isPostiveOrNegative2 } from "./desafios/number/8kyu/isPositveOrNegative";
+import { squareIntegers, calculateSumAndAverage, numbersDivisibleByFour, sumFirstHundredNumbers } from "./desafios/number/8kyu/loopForII";
+
+//strings
+import { isPalindromo } from "./desafios/string/7kyu/isPalindromo";
+import { startsWithLetter } from "./desafios/string/8kyu/startsWithLetter";
+
+/*
+    EXEMPLO
+    Chamar a função aqui, algumas precisam informar dados nos parametros.
+
+    fibonacciNumbers()
+*/
