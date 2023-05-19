4d7a7905013dc5ba5d2ac5c8adae0d81376a48f7
richzago97 <rich.czago@gmail.com>
2023-05-19 11:20:30 -0300

    feat: adicionado nova função de array

--- desafios/array/8kyu/arraysII.ts
+++ desafios/array/8kyu/arraysII.ts
@@ -9,11 +9,9 @@
 */
 
 const list_a: number[] = [
-    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15,
-    16, 17, 18, 19, 20, 21, 22, 23, 24, 25,
-    26, 27, 28, 29, 30
+    1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21,
+    22, 23, 24, 25, 26, 27, 28, 29, 30,
 ];
-
 export const countEvenOdd = (numbers: number[]): string => {
     let countEven: number = 0;
     let countOdd: number = 0;
@@ -43,9 +41,30 @@ export const countEvenOdd2 = (numbers: number[]) => {
     return `A lista recebida possui ${countEven} elementos pares e ${countOdd}
     ímpares existentes na lista recebida.
     `;
-
 };
 
+/*
+    Escreva uma função que recebe uma lista b de tamanho 10, composta por números inteiros.⁠
+    Retorne o total de elementos ímpares existentes na matriz e também o percentual do valor total 
+    de números ímpares em relação à quantidade total de elementos armazenados na matriz.
+*/
 
+const list_b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
+export const countOddPercentualValue = (numbers: number[]) => {
+    let totalOdd: number = 0;
+    let totalCountArray: number = 0;
+    let totalSumOdd: number = 0;
 
+    numbers.forEach((n) => {
+        if (n % 2 === 1) {
+            totalOdd += 1;
+            totalSumOdd += n;
+        }
+        totalCountArray += 1;
+    });
+
+    const percentual = (totalSumOdd / totalCountArray) * 100;
+
+    return `O número total de elementos ímpares existentes é ${totalOdd}, e o percentual do valor total é: ${percentual}`;
+};
