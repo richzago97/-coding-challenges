aa648381a25d392c3a54c4e4f60757ed0bd64d47
richzago97 <rich.czago@gmail.com>
2023-05-19 11:21:02 -0300

    feat: adicionado novo import

--- index.ts
+++ index.ts
@@ -1,17 +1,47 @@
 //numbers
-import { fibonacciNumbers, fibonacci, sumGrains } from "./desafios/number/7kyu/loopForIII";
+import {
+    fibonacciNumbers,
+    fibonacci,
+    sumGrains,
+} from "./desafios/number/7kyu/loopForIII";
 import { checkHourAvailable } from "./desafios/number/8kyu/checkHourAvailable";
 import { isBigger, isBigger2 } from "./desafios/number/8kyu/isBigger";
-import { isPositiveOrNegative, isPostiveOrNegative2 } from "./desafios/number/8kyu/isPositveOrNegative";
-import { squareIntegers, calculateSumAndAverage, numbersDivisibleByFour, sumFirstHundredNumbers } from "./desafios/number/8kyu/loopForII";
+import {
+    isPositiveOrNegative,
+    isPostiveOrNegative2,
+} from "./desafios/number/8kyu/isPositveOrNegative";
+import {
+    squareIntegers,
+    calculateSumAndAverage,
+    numbersDivisibleByFour,
+    sumFirstHundredNumbers,
+} from "./desafios/number/8kyu/loopForII";
 
 //strings
 import { isPalindromo } from "./desafios/string/7kyu/isPalindromo";
 import { startsWithLetter } from "./desafios/string/8kyu/startsWithLetter";
 
 //arrays
-import { myArrMinFive, myArrMinFive2, checkPositionExists, checkPositionExists2, checkPositionExists3, isPositionEvenOrOdd, isPositionEvenOrOdd2, checkBiggerName, checkBiggerName2, sumLastTwoPositionsMutipleOfTwo, checkIfNameAlreadyExists, whichArrayIsBigger, whichArrayIsBigger2 } from "./desafios/array/8kyu/arraysI";
-import { countEvenOdd, countEvenOdd2 } from "./desafios/array/8kyu/arraysII";
+import {
+    myArrMinFive,
+    myArrMinFive2,
+    checkPositionExists,
+    checkPositionExists2,
+    checkPositionExists3,
+    isPositionEvenOrOdd,
+    isPositionEvenOrOdd2,
+    checkBiggerName,
+    checkBiggerName2,
+    sumLastTwoPositionsMutipleOfTwo,
+    checkIfNameAlreadyExists,
+    whichArrayIsBigger,
+    whichArrayIsBigger2,
+} from "./desafios/array/8kyu/arraysI";
+import {
+    countEvenOdd,
+    countEvenOdd2,
+    countOddPercentualValue,
+} from "./desafios/array/8kyu/arraysII";
 /*
     EXEMPLO:
 
