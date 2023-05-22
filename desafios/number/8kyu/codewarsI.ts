933f02ce72f574948eea06d79202289058e62265
richzago97 <rich.czago@gmail.com>
2023-05-22 14:46:01 -0300

    feat: criado nova função de numbers 8kyu

--- desafios/number/8kyu/codewarsI.ts
+++ desafios/number/8kyu/codewarsI.ts
@@ -49,4 +49,17 @@ export function oddCount(n: number): number {
     return result;
 }
 
+/*
+    Summation
+    Write a program that finds the summation of every 
+    number from 1 to num. The number will always be 
+    a positive integer greater than 0.
+*/
 
+export const summation = (num: number) => {
+    let sum = 0;
+    for (let i = 0; i <= num; i++) {
+        sum += i;
+    }
+    return sum;
+};
