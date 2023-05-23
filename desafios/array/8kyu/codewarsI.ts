ef1dd4ce8f181c8b37dbfa3284468a327380d3fd
richzago97 <rich.czago@gmail.com>
2023-05-23 10:41:06 -0300

    feat: criado nova função

--- desafios/array/8kyu/codewarsI.ts
+++ desafios/array/8kyu/codewarsI.ts
@@ -149,3 +149,24 @@ export const max2 = (list: number[]): number => {
     }
     return biggerValue;
 };
+
+/*
+    You take your son to the forest to see the monkeys. You know that there are a certain number there
+     (n), but your son is too young to just appreciate the full number, he has to start 
+     counting them from 1.
+
+    As a good parent, you will sit and count with him. Given the number (n), populate an 
+    array with all numbers up to and including that number, but excluding zero.
+
+    For example(Input --> Output):
+
+    10 --> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+    1 --> [1]
+*/
+export function monkeyCount(n: number): number[] {
+    let result: number[] = [];
+    for (let i = 1; i <= n; i++) {
+        result.push(i);
+    }
+    return result;
+}
