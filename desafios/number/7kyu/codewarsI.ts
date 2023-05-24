044f879fbc579abe65ef29da622c301d09ebbe5f
richzago97 <rich.czago@gmail.com>
2023-05-24 16:33:23 -0300

    feat: adicionado nova função 7kyu

--- desafios/number/7kyu/codewarsI.ts
+++ desafios/number/7kyu/codewarsI.ts
@@ -27,3 +27,38 @@
 export function cookingTime(eggs: number): number {
     return Math.ceil(eggs / 8) * 5;
 }
+
+/*
+    DESCRIPTION:
+        In this kata you will have to write a function that takes litres and
+        pricePerLitre (in dollar) as arguments.
+
+        Purchases of 2 or more litres get a discount of 5 cents per litre, 
+        purchases of 4 or more litres get a discount of 10 cents per litre,
+        and so on every two litres, up to a maximum discount of 25 cents per litre.
+        But total discount per litre cannot be more than 25 cents. Return
+        the total cost rounded to 2 decimal places. Also you can guess that there
+        will not be negative or non-numeric inputs.
+
+    Good Luck!
+
+    Note
+    1 Dollar = 100 Cents
+*/
+
+export const fuelPrice = (litres: number, pricePerLitre: number): number => {
+    let totalCost = litres * pricePerLitre;
+    let discount = 0;
+
+    if (litres >= 10) {
+        discount = 0.25;
+    } else {
+        discount = Math.floor(litres / 2) * 0.05;
+    }
+
+    totalCost = (pricePerLitre - discount) * litres;
+
+    totalCost = parseFloat(totalCost.toFixed(2));
+
+    return totalCost;
+};
