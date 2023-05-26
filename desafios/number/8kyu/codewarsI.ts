76c516022e5e0cefb463a406afd6397fb8bb18be
richzago97 <rich.czago@gmail.com>
2023-05-26 14:13:57 -0300

    feat: criado nova função

--- desafios/number/8kyu/codewarsI.ts
+++ desafios/number/8kyu/codewarsI.ts
@@ -212,3 +212,31 @@ export const makeNegative = (num: number): number => {
     }
     return num;
 };
+
+/*
+    After a hard quarter in the office you decide to get some rest on a vacation.
+    So you will book a flight for you and your girlfriend and try to leave all 
+    the mess behind you.
+
+    You will need a rental car in order for you to get around in your vacation. 
+    The manager of the car rental makes you some good offers.
+
+    Every day you rent the car costs $40. If you rent the car for 7 or more days,
+    you get $50 off your total. Alternatively, if you rent the car for 3 or
+    more days, you get $20 off your total.
+
+    Write a code that gives out the total amount for different days(d).
+*/
+
+export function rentalCarCost(d: number): number {
+    let rentPrice = 40;
+
+    if (d < 3) {
+        return rentPrice * d;
+    }
+
+    if (d >= 3 && d <= 6) {
+        return rentPrice * d - 20;
+    }
+    return rentPrice * d - 50;
+}
