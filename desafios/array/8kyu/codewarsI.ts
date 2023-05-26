cc215b75f012746bdc836c028f5555ec41e132f6
richzago97 <rich.czago@gmail.com>
2023-05-26 14:13:11 -0300

    feat: criado nova função

--- desafios/array/8kyu/codewarsI.ts
+++ desafios/array/8kyu/codewarsI.ts
@@ -209,3 +209,23 @@ export const squareOrSquareRoot2 = (array: number[]): number[] => {
         Math.sqrt(elem) % 1 == 0 ? Math.sqrt(elem) : elem * elem
     );
 };
+
+/*
+    Given an array of integers as strings and numbers, return the sum
+    of the array values as if all were numbers.
+
+    Return your answer as a number.
+*/
+
+export function sumMix(x: any[]): number {
+    const arrayNumbers: number[] = x.map((element) => Number(element));
+
+    const result = arrayNumbers.reduce(
+        (acc, currentValue) => acc + currentValue,
+        0
+    );
+
+    return result;
+}
+
+console.log(sumMix([9, 3, "7", "3"]));
