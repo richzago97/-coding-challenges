bf3a0dc6500ca5a632c975633234f6650d385af0
richzago97 <rich.czago@gmail.com>
2023-06-05 10:01:27 -0300

    feat: nova função 8kyu

--- desafios/number/8kyu/codewarsI.ts
+++ desafios/number/8kyu/codewarsI.ts
@@ -260,3 +260,29 @@ export function countBy(x: number, n: number): number[] {
 
    return result;
 }
+
+// Your Job
+// Find the sum of all multiples of n below m
+
+// Keep in Mind
+// n and m are natural numbers (positive integers)
+// m is excluded from the multiples
+// Examples
+// sumMul(2, 9)   ==> 2 + 4 + 6 + 8 = 20
+// sumMul(3, 13)  ==> 3 + 6 + 9 + 12 = 30
+// sumMul(4, 123) ==> 4 + 8 + 12 + ... = 1860
+// sumMul(4, -7)  ==> "INVALID"
+
+export function sumMul(n: number, m: number) {
+   let result = 0;
+
+   if (n <= 0 || m <= 0) {
+      return "INVALID";
+   }
+
+   for (let i = n; i < m; i += n) {
+      result += i;
+   }
+
+   return result;
+}
